8808df5ef027714d420d4dfff14f42f597b25e62
Liz <elizabeth.pinero.r@gmail.com>
2024-01-20 09:37:51 -0500

    Added single enemy

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function() {
     const ctx = canvas.getContext('2d')
     canvas.width = 800
     canvas.height = 720
+    let enemies = []
 
     // Applies event listeners to keyboard & keep track of keys that are pressed down
     class InputHandler {
@@ -67,8 +68,8 @@ window.addEventListener('load', function() {
                 this.speed = 5
             } else if (input.keys.indexOf('ArrowLeft') > -1) {
                 this.speed = -5
-            } else if(input.keys.indexOf('ArrowUp') > -1) {
-                this.vy -= 5
+            } else if(input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+                this.vy -= 30
             } else {
                 this.speed = 0
             }
@@ -84,8 +85,10 @@ window.addEventListener('load', function() {
             // If player is in air, gradually add more weight to velocity y, else if player is back on ground, set vy back to 0
             if(!this.onGround()) {
                 this.vy += this.weight
+                this.frameY = 1
             } else {
                 this.vy = 0
+                this.frameY = 0
             }
             if(this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
 
@@ -96,15 +99,60 @@ window.addEventListener('load', function() {
     }
     
     class Background {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth
+            this.gameHeight = gameHeight
+            this.image = document.getElementById('backgroundImage')
+            this.x = 0
+            this.y = 0
+            this.width = 2400
+            this.height = 720
 
+            this.speed = 20
+        }
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y, this.width, this.height)
+            context.drawImage(this.image, this.x + this.width, this.y, this.width, this.height)
+        }
+        update() {
+            this.x -= this.speed
+            if(this.x < 0 - this.width) this.x = 0
+        }
     }
 
     class Enemy {
+        constructor(gameWidth, gameHeight) {
+            this.gameWidth = gameWidth
+            this.gameHeight = gameHeight
+            this.width = 160
+            this.height = 119
+            this.image = document.getElementById('enemyImage')
+            this.x = this.gameWidth
+            this.y = this.gameHeight - this.height
 
+            this.frameX = 0
+            this.speed = 8
+        }
+        draw(context) {
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+        }
+        update() {
+            this.x -= this.speed
+        }
     }
+    enemies.push(new Enemy(canvas.width, canvas.height))
 
-    function handleEnemies() {
-
+    function handleEnemies(deltaTime) {
+        if(enemyTimer > enemyInterval + randomEnemyInterval) {
+            enemies.push(new Enemy(canvas.width, canvas.height))
+            enemyTimer = 0
+        } else {
+            enemyTimer += deltaTime
+        }
+        enemies.forEach(enemy => {
+            enemy.draw(ctx)
+            enemy.update()
+        })
     }
 
     function displayStatusText() {
@@ -113,13 +161,29 @@ window.addEventListener('load', function() {
 
     const input = new InputHandler()
     const player = new Player(canvas.width, canvas.height)
+    const background = new Background(canvas.width, canvas.height)
 
-    function animate() {
+    let lastTime = 0
+    let enemyTimer = 0
+    let enemyInterval = 1000
+    let randomEnemyInterval = Math.random() * 1000 + 500
+
+    function animate(timeStamp) {
+        // how many milliseconds our computer needs to serve 1 animation frame
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp
+
         ctx.clearRect(0, 0, canvas.width, canvas.height)
+        background.draw(ctx)
+        // background.update()
         player.draw(ctx)
         player.update(input)
+
+        handleEnemies(deltaTime)
+
+        // this function automatically passes timeStamp as arg to animate.
         requestAnimationFrame(animate)
     }
 
-    animate()
+    animate(0)
 })
